eecd601fa0b5aa83c2b632dfb507603697d16148
yuqin wang <3186831775@qq.com>
2025-09-02 20:10:40 +0800

    完成launch调用放包和fastlivo2

---
+++ ros_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/yutou/fastlivo2/ros_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/yutou/fastlivo2/ros_ws/devel;/home/yutou/fastlivo2/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/yutou/fastlivo2/ros_ws/devel;/home/yutou/fastlivo2/catkin_ws/devel;/opt/ros/noetic' != '' else []
